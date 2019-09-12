c99b7b5fa74c46165b0aa467e78eeada3aea1bfd
Class Account <cs199-fdc@hpse-14.EECS.Berkeley.EDU>
2019-09-11 17:59:33 -0700

    added max_w1 as input, cleaned up code to remove error warnings when running

--- REG_seriesN_jacksonPaddock.py
+++ REG_seriesN_jacksonPaddock.py
@@ -38,7 +38,8 @@ def design_seriesN_reg_eqn(db_n, sim_env,
         ibias, vdd, vref, vb_n,
         cload, rload, rsource,
         vg_res, psrr_min, pm_min, err_max,
-        linereg_max, loadreg_max, delta_v_lnr, delta_i_ldr):
+        linereg_max, loadreg_max, delta_v_lnr, delta_i_ldr,
+        max_w1):
     """
     Designs an LDO with an op amp abstracted as a voltage-controlled voltage
     source and source resistance. Equation-based.
@@ -55,13 +56,14 @@ def design_seriesN_reg_eqn(db_n, sim_env,
         vg_res:       Float. Step resolution when sweeping transistor gate voltage.
         psrr_min:     Float. Minimum PSRR.
         pm_min:       Float. Minimum phase margin.
-        err_max       Float. Maximum percent static error at output.
+        err_max       Float. Maximum percent static error at output (as decimal).
         linereg_max   Float. Maximum percent change in output voltage given
-                             change in input voltage.
+                             change in input voltage (as decimal).
         loadreg_max   Float. Maximum percent change in output voltage given
-                             change in output current.
+                             change in output current (as decimal).
         delta_v_lnr   Float. Given change in input voltage to calculate line reg.
         delta_i_ldr   Float. Given change in output current to calculate load reg.
+        max_w1        Float. Maximum angular frequency of lower op amp pole.
 
     Raises:
         ValueError: If unable to meet the specification requirements.
@@ -103,16 +105,19 @@ def design_seriesN_reg_eqn(db_n, sim_env,
 
     # Find location of loop gain pole with determined parameters.
     wa = (ro + rsource + rload + gm*ro*rload)/(rload*cload*(ro + rsource))
+    print("wa={}\n".format(wa))
     # Minimum op amp gain using loop gain equation and static error bound.
     Ao_min = max((ro + rsource + rload + gm*ro*rload)/(gm*ro*rload), vg/(err_max*vref))
 
     ## Determine w1 and w2 of op amp
     # Sweep op amp lower pole magnitude and determine other parameters.
-    stop = 8 #TODO: Define upper limit on pole frequencies.
     designs = []
-    for w1 in np.logspace(1,stop):
-        # Choose third pole to interfere less with second pole. TODO: Better way?
-        w2 = 100*max(wa, w1)
+    for w1 in np.logspace(1,np.log10(max_w1)):
+        # Choose op amp pole 2 to separate second and third poles. TODO: Better way?
+        if max(w1,wa) > 100*min(w1,wa):
+            w2 = max(w1,wa)/100
+        else:
+            w2 = max(w1,wa)*100
 
         ## Define helper functions to find corresponding Ao and wo.
         def Ao_to_wo(Ao):
@@ -120,12 +125,13 @@ def design_seriesN_reg_eqn(db_n, sim_env,
             b = (w1**2 + w2**2 + wa**2)/(w1*w2*wa)**2
             c = 1/w1**2 + 1/w2**2 + 1/wa**2
             d = 1 - (gm*ro*rload*Ao/(ro + rsource + rload + gm*ro*rload))**2
-            roots = np.sqrt(np.roots([a,b,c,d]))
-            return roots[2]
-            for root in roots:
-                if np.isreal(root):
-                    
-                    return root
+            roots_sq = np.roots([a,b,c,d])
+            # TODO: Fix this.
+            #if roots[2] < 10**7 and Ao <200:
+            #return roots[2]
+            for root_sq in roots_sq:
+                if root_sq > 0:
+                    return np.sqrt(root_sq)
 
         def wo_to_Ao(wo):
             return (ro + rsource + rload + gm*ro*rload)/(gm*ro*rload)*np.sqrt((1+(wo/w1)**2)*(1+(wo/w2)**2)*(1+(wo/wa)**2))
@@ -142,7 +148,7 @@ def design_seriesN_reg_eqn(db_n, sim_env,
         Ao_max = wo_to_Ao(wo_max)
 
         # Use PSRR bound to possibly tighten lower bound on Ao.
-        if psrr_min > ro + rsource + rload:
+        if psrr_min > ro + rsource + rload + gm*ro*rload:
             psrr_min_wo = wa*np.sqrt((psrr_min/(ro + rsource + rload + gm*ro*rload))**2 - 1)
             Ao_min = max(Ao_min, wo_to_Ao(psrr_min_wo))
         # Make sure bounds are valid.
@@ -155,9 +161,6 @@ def design_seriesN_reg_eqn(db_n, sim_env,
             ## TODO: Check if design is plausible (different file?).
 
             ## Check remaining bounds.
-
-
-        
             # Variables for transfer function and output resistance equations.
             a2 = ro + rsource + rload + gm*ro*rload
             b2 = gm*ro*rload*Ao + a2
@@ -178,10 +181,10 @@ def design_seriesN_reg_eqn(db_n, sim_env,
                 a3 = 3*c**2
                 b3 = - 4*c2*d2*(c2 + 2*a2*e2) + 2*(c2*d2*e2)**2 + 2*a2**2
                 c3 = 2*c2*d2**2*e2*(b2 + a2) + (a2*d2*e2)**2 + (c2*d2)**2 - 2*a2*d2*b2
-                deriv_roots2 = np.sqrt(np.roots([a3, b3, c3]))
-                for root in deriv_roots2:
-                    if np.isreal(root): #TODO: Should root only be considered if less than wo?
-                        H_max = max(H_max, H(root))
+                deriv_roots2_sq = np.roots([a3, b3, c3])
+                for root_sq in deriv_roots2_sq:
+                    if root_sq > 0: #TODO: Should root only be considered if less than wo?
+                        H_max = max(H_max, H(np.sqrt(root_sq)))
                 # Find maximum magnitude of output impedance in [0, wo].
                 rout = lambda x: (ro + rsource)/(gm*ro*Ao)*H(x)*np.sqrt((1+(x/w1)**2)*(1+(x/w2)**2))
                 rout_max = max(rout(0),rout(wo))
@@ -190,10 +193,10 @@ def design_seriesN_reg_eqn(db_n, sim_env,
                 c4 = 3*f2*(c2*e2)**2 - 10*a2*c2*e2*f2/d2 + 3*f2*(a2/d2)**2 + 12*(c2/d2)**2
                 d4 = 2*c2*e2*f2*(a2 + b2) + f2*(a2*e2)**2 + f2*c2**2 + 4*(c2*e2)**2 - 2*a2*(b2*f2 + 8*c2*e2)/d2 + (4*a2**2 - 2*b2**2)/d2**2
                 e4 = 4*c2*e2*(a2 + b2) + 2*(a2*e2)**2 + 2*c2**2 - 4*a2*b2/d2 - b2**2*f2
-                deriv_roots3 = np.sqrt(np.roots([a4, b4, c4, d4, e4]))
-                for root in deriv_roots3:
-                    if np.isreal(root): #TODO: Should root only be considered if less than wo?
-                        rout_max = np.real(max(rout_max, rout(root)))
+                deriv_roots3_sq = np.roots([a4, b4, c4, d4, e4])
+                for root_sq in deriv_roots3_sq:
+                    if root_sq > 0: #TODO: Should root only be considered if less than wo?
+                        rout_max = max(rout_max, rout(np.sqrt(root_sq)))
             # Check if parameters fit given bounds.
             fits_linereg, fits_loadreg = False, False
             linereg = H_max*delta_v_lnr / vref
@@ -217,7 +220,7 @@ def design_seriesN_reg_eqn(db_n, sim_env,
                 #print("Not all bounds met.\n")
                 pass
                 
-    if designs == []:
+    if len(designs) == 0:
         print("FAIL. No solutions found.")
         return
     else:
@@ -302,17 +305,18 @@ def run_main():
 	vdd=1.0,
 	vref=0.5,
 	vb_n=0,
-	cload=20e-14,
+	cload=2e-15,
 	rload=1e6,
-	rsource=100,
+	rsource=1000,
         vg_res=0.001,
 	psrr_min=1,
         pm_min=45,
-        err_max=0.05,
+        err_max=0.01,
 	linereg_max=0.02,
 	loadreg_max=0.05,
 	delta_v_lnr=1e-3,
-	delta_i_ldr=1e-9
+	delta_i_ldr=1e-9,
+        max_w1=1e8
         )
 
     amp_specs = design_seriesN_reg_eqn(**specs)
